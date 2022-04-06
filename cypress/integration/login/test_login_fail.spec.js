38d5ba4a9e38423e9baf1309d4b6bd0317c956d1
geralddcx <gerald.ducloux@gmail.com>
2022-04-06 11:23:26 +0200

    new file in login

---
+++ cypress/integration/login/test_login_fail.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+beforeEach( () => {
+    cy.visit('https://preprod.backmarket.fr/register')
+
+    cy.get('[data-qa="accept-cta"]').click();
+})
+
+describe('Un utilisateur veut se connecter avec un mot de passe incorrect', () => {
+    it('il remplit le champ mail correctement', () => {
+        cy.get("#signin-email").type('grainsurlaplage@gmail.com');
+        cy.get("#signin-email").should('include.value','grainsurlaplage@gmail.com');
+
+        cy.get("#signin-password").type('mauvaisMotDePasse');
+        cy.get("#signin-password").should('include.value', 'mauvaisMotDePasse');
+
+        cy.get('[data-qa="signin-submit-button"]').click();
+        
+        cy.contains("Informations d'identification erronées").should('be.visible');
+        cy.url().should('eq','https://preprod.backmarket.fr/register');
+    })
+})
